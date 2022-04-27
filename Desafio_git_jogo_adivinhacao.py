1c493abc0bc5e23a9bc8561ab0aef77e3999edc2
VandersonDosSantos <98714171+VandersonDosSantos@users.noreply.github.com>
2022-04-27 15:44:43 -0300

    Update Desafio_git_jogo_adivinhacao.py

--- Desafio_git_jogo_adivinhacao.py
+++ Desafio_git_jogo_adivinhacao.py
@@ -2,12 +2,24 @@ import random
 
 print('JOGO DE ADIVINHAÇÃO')
 
-numero_escolhido = int(input('Digite um número entre 0 e 20: '))
+tentativas = 5
 
 numero_secreto = random.randint(0 , 21)
 
-if numero_escolhido == numero_secreto:
-    print(f'O número era {numero_secreto} você acertou')
+while tentativas > 0:
+    
+    numero_escolhido = int(input('Digite um número entre 0 e 20: '))
 
-else:
-    print(f'O número correto era {numero_secreto} você errou!')
+    if numero_escolhido == numero_secreto:
+        print(f'O número era {numero_secreto} você acertou!')
+        break
+
+    elif numero_escolhido < numero_secreto:
+        print('Você errou, o número secreto é maior que o seu palpite!')
+        
+    elif numero_escolhido > numero_secreto:
+        print('Você errou, o número secreto é menor que o seu palpite!')
+        
+    tentativas -= 1
+    print(f'Você tem {tentativas} tentativas.')
+print('FIM DO JOGO!')
